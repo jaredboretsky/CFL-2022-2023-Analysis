f0eaaf01e0ae07ad120dfc01e778ce41bd881236
jaredboretsky <50340896+jaredboretsky@users.noreply.github.com>
2023-12-01 13:15:41 -0500

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -14,7 +14,7 @@ import base64
 import random
 
 
-# In[10]:
+# In[17]:
 
 
 # Initialize Dash app
@@ -31,7 +31,7 @@ def update_scatter_plot(selected_week, selected_metric):
     # Choose the plot based on the selected metric
     if selected_metric == 'Points':
         # Define the path to the CSV file for the selected week
-        csv_path = f"data/week_{selected_week}_2023/scoring_breakdown.csv"
+        csv_path = f"/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{selected_week}_2023/scoring_breakdown.csv"
         # Check if the file exists before attempting to read it
         if os.path.exists(csv_path):
             # Read the data from the CSV file for the selected week
@@ -78,8 +78,8 @@ def update_scatter_plot(selected_week, selected_metric):
 
         return fig_points
     else:
-        csv_path_team = f"data/week_{selected_week}_2023/net_offence.csv"
-        csv_path_opp = f"data/week_{selected_week}_2023/opponent_net_offence.csv"
+        csv_path_team = f"/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{selected_week}_2023/net_offence.csv"
+        csv_path_opp = f"/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{selected_week}_2023/opponent_net_offence.csv"
     # Check if the file exists before attempting to read it
         if os.path.exists(csv_path_team):
             # Read the data from the CSV file for the selected week
@@ -139,9 +139,9 @@ def update_scatter_plot(selected_week, selected_metric):
     Output('first-down', 'figure'),
     [Input('week-dropdown-first-down', 'value')])
     
-def update_chart(week_number):
+def update_first_down(week_number):
     # Load data for the selected week
-    filename = f'data/week_{week_number}_2023/first_down_offence.csv'
+    filename = f'/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{week_number}_2023/first_down_offence.csv'
     df = pd.read_csv(filename)
     # Perform the same data processing as before
     df['1st_down_pass_calls'] = pd.to_numeric(df['1st_down_pass_calls'], errors='coerce')
@@ -225,6 +225,334 @@ def update_chart(week_number):
             )
 
     return {'data': data, 'layout': layout}
+
+@app.callback(
+    Output('agression', 'figure'),
+    [Input('week-dropdown-agression', 'value')])
+    
+def update_agression(week_number):
+    filename = f'/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/agression_data.csv'
+    df = pd.read_csv(filename)
+    df = df[df['Week'] == week_number]
+    df['PF_per_game'] = df['PF']/df['GP']
+    # Bar chart creation code (same as before)
+    data = []
+    bar_width = 0.4
+    fig = px.bar(df, x='Team', y=['C2_att', '20+_yds_Att_per_game', 'Att', 'PF_per_game'], barmode='group')
+    
+    fig.for_each_trace(lambda t: t.update(name = {
+        'C2_att': '2-Point Conversion Attempts',
+        '20+_yds_Att_per_game': 'Deep Passes (20+ Yards) Attempted Per Game',
+        'Att': '3rd Down Conversion Attempts',
+        'PF_per_game': 'Points For Per Game'
+    }[t.name]))
+    
+    fig.update_layout(
+        title='Team Metrics by Week',
+        xaxis_title='Teams',
+        yaxis_title='Values',
+        legend_title='Legend',
+        barmode='group'
+    )
+
+    return fig
+
+@app.callback(
+    Output('second-down', 'figure'),
+    [Input('week-dropdown-second-down', 'value'),
+     Input('heatmap-type', 'value')])
+    
+def update_chart(week_number, heatmap_type):
+    if heatmap_type == 'offense':
+        offense_file_name = f'/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{week_number}_2023/second_down_conversions.csv'
+        df = pd.read_csv(offense_file_name)
+    else:
+        defense_file_name = f'/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_{week_number}_2023/opponent_second_down_conversions.csv'
+        df = pd.read_csv(defense_file_name)
+    
+    # Create two separate dataframes
+    df_percentages = df[['Team', '1-3_yds_%', '4-6_yds_%', '7+_yds_%']].set_index('Team')
+    df_yards_to_go = df[['Team', 'Yds_to_go_Avg']].set_index('Team')
+    
+    # Create two separate heatmaps
+    fig = make_subplots(
+        rows=1, cols=2,
+        shared_yaxes=True,
+        horizontal_spacing=0.001,  # Spacing between heatmaps
+        column_widths=[0.7, 0.3],  # Adjusts the relative widths of the two heatmaps
+        specs=[[{"type": "heatmap"}, {"type": "heatmap"}]]
+    )
+
+    # Assuming df_percentages and df_yards_to_go are your two DataFrames
+    # Add the percentage heatmap as the first subplot
+    
+    fig.add_trace(
+        go.Heatmap(
+            z=df_percentages.values,
+            x=df_percentages.columns,
+            y=df_percentages.index,
+            coloraxis="coloraxis1"
+        ),
+        row=1, col=1
+    )
+
+    # Add the yards to go heatmap as the second subplot
+    fig.add_trace(
+        go.Heatmap(
+            z=df_yards_to_go.values,
+            x=df_yards_to_go.columns,
+            y=df_yards_to_go.index,
+            coloraxis="coloraxis2"
+        ),
+        row=1, col=2
+    )
+# Update the layout for the subplot figure
+    if heatmap_type == 'offense':
+        fig.update_layout(
+            coloraxis1=dict(colorscale='Blues', colorbar=dict(title='Percentage', x=-.2)),  # Color scale and legend for 1st heatmap
+            coloraxis2=dict(colorscale='Greens', colorbar=dict(title='Yards to Go', x=1), reversescale=True),   # Color scale and legend for 2nd heatmap
+            title_text="Team Efficiency on 2nd Down",
+            showlegend=False
+        )
+    else:
+        fig.update_layout(
+            coloraxis1=dict(colorscale='Blues', colorbar=dict(title='Percentage', x=-.2), reversescale=True),  # Color scale and legend for 1st heatmap
+            coloraxis2=dict(colorscale='Greens', colorbar=dict(title='Yards to Go', x=1)),   # Color scale and legend for 2nd heatmap
+            title_text="Opponent Efficiency on 2nd Down",
+            showlegend=False
+        )
+
+# Move the column names to the top
+    fig.update_xaxes(side="top", row=1, col=1)
+    fig.update_xaxes(side="top", row=1, col=2)
+
+    
+# Create the Dash layout using the combined figure
+    app.layout = html.Div([
+        html.H1("CFL Team Efficiency on 2nd Down"),
+        dcc.Graph(
+            id='combined_heatmap',
+            figure=fig
+        )
+    ])
+    
+    return fig
+
+def create_big_play_analysis_graph():
+    df = pd.read_csv('/Users/jaredboretsky/Documents/concordia-bootcamps/ds-final_project/CFL_Data/week_21_2023/big_play_analysis.csv')
+    df.drop(9, inplace=True)
+    
+    df['Offensive_Cumsum'] = df['Total']
+    df['Defensive_Cumsum'] = df['Opp_off_20+_Rush'] + df['Opp_off_30+_Pass']+df['Opp_kick_Punt_Rets']+df['Opp_kick_K/O_Rets']+df['Opp_kick_FGM_Ret']
+    max_offensive = df[['Offensive_Cumsum']].sum(axis=1).max()
+    max_defensive = df[['Defensive_Cumsum']].sum(axis=1).max()
+    max_value = max(max_offensive, max_defensive)
+
+    # Offset for creating whitespace in the middle
+    offset = 5
+    max_tick = max_value
+
+    tick_values_positive = list(range(5, max_tick + 10, 5))
+    tick_values_negative = [-tick for tick in tick_values_positive]
+    tick_values = tick_values_negative + tick_values_positive
+
+    # Generate tick labels. These will be the text labels that appear at the tick marks.
+    # For the positive side, we subtract the offset to start the labels at 0.
+    # For the negative side, we add the offset to start the labels at 0.
+    tick_labels_negative = [str((tick + 5)*-1) for tick in tick_values_negative]
+    tick_labels_positive = [str(tick - 5) for tick in tick_values_positive]
+    tick_labels = tick_labels_negative + tick_labels_positive
+
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                name='Rushes (20+ Yards)',
+                y=df['Team'],
+                x=df['20+_Rush'],
+                orientation='h',
+                base = offset,
+                marker={'color': 'blue'},
+                hoverinfo='text',
+                text=['Team: {}<br>Rushes (20+ Yards): {}<br>Total Big Plays: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['20+_Rush'], df['Offensive_Cumsum'])]
+            ),
+            go.Bar(
+                name='Passes (30+ Yards)',
+                y=df['Team'],
+                x=df['30+_Pass'],
+                orientation='h',
+                base=df['20+_Rush']+offset,
+                marker={'color': 'red'},
+                hoverinfo='text',
+                text=['Team: {}<br>Passes (30+ Yards): {}<br>Total Big Plays: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['30+_Pass'], df['Offensive_Cumsum'])]
+            ),
+            go.Bar(
+                name='Punt Returns',
+                y=df['Team'],
+                x=df['Punt_Rets'],
+                orientation='h',
+                base=df['20+_Rush']+df['30+_Pass']+offset,
+                marker={'color': 'Green'},
+                hoverinfo='text',
+                text=['Team: {}<br>Punt Returns: {}<br>Total Big Plays: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Punt_Rets'], df['Offensive_Cumsum'])]
+            ),
+            go.Bar(
+                name='Kickoff Returns',
+                y=df['Team'],
+                x=df['K/O_Rets'],
+
+                orientation='h',
+                base=df['20+_Rush']+df['30+_Pass']+df['Punt_Rets']+offset,
+                marker={'color': 'Brown'},
+                hoverinfo='text',
+                text=['Team: {}<br>Kickoff Returns: {}<br>Total Big Plays: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['K/O_Rets'], df['Offensive_Cumsum'])]
+            ),
+            go.Bar(
+                name='Missed Field Goal Returns',
+                y=df['Team'],
+                x=df['FGM_Rets'],
+                orientation='h',
+                base=df['20+_Rush']+df['30+_Pass']+df['Punt_Rets']+df['K/O_Rets']+offset,
+                marker={'color': 'Orange'},
+                hoverinfo='text',
+                text=['Team: {}<br>Missed Field Goal Returns: {}<br>Total Big Plays Plays: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['FGM_Rets'], df['Offensive_Cumsum'])]
+            ),
+            go.Bar(
+                name='Rushes (20+ Yards)',
+                y=df['Team'],
+                x=-df['Opp_off_20+_Rush'],
+                orientation='h',
+                base = -offset,
+                marker={'color': 'blue'},
+                hoverinfo='text',
+                text=['Team: {}<br>Rushes (20+ Yards) Allowed: {}<br>Total Big Plays Plays Allowed: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Opp_off_20+_Rush'], df['Defensive_Cumsum'])],
+                showlegend=False
+            ),
+            go.Bar(
+                name='Passes (30+ Yards)',
+                y=df['Team'],
+                x=-df['Opp_off_30+_Pass'],
+                orientation='h',
+                base=-df['Opp_off_20+_Rush']-offset,
+                marker={'color': 'red'},
+                hoverinfo='text',
+                text=['Team: {}<br>Passes (30+ Yards) Allowed: {}<br>Total Big Plays Plays Allowed: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Opp_off_30+_Pass'], df['Defensive_Cumsum'])],
+                showlegend=False
+            ),
+            go.Bar(
+                name='Punt Returns',
+                y=df['Team'],
+                x=-df['Opp_kick_Punt_Rets'],
+                orientation='h',
+                base=-df['Opp_off_20+_Rush']-df['Opp_off_30+_Pass']-offset,
+                marker={'color': 'Green'},
+                hoverinfo='text',
+                text=['Team: {}<br>Punt Returns Allowed: {}<br>Total Big Plays Plays Allowed: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Opp_kick_Punt_Rets'], df['Defensive_Cumsum'])],
+                showlegend=False
+            ),
+            go.Bar(
+                name='Kickoff Returns',
+                y=df['Team'],
+                x=-df['Opp_kick_K/O_Rets'],
+                orientation='h',
+                base=-df['Opp_off_20+_Rush']-df['Opp_off_30+_Pass']-df['Opp_kick_Punt_Rets']-offset,
+                marker={'color': 'Brown'},
+                hoverinfo='text',
+                text=['Team: {}<br>Kickoff Returns Allowed: {}<br>Total Big Plays Plays Allowed: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Opp_kick_K/O_Rets'], df['Defensive_Cumsum'])],
+                showlegend=False
+            ),
+            go.Bar(
+                name='Missed Field Goal Returns',
+                y=df['Team'],
+                x=-df['Opp_kick_FGM_Ret'],
+                orientation='h',
+                base=-df['Opp_off_20+_Rush']-df['Opp_off_30+_Pass']-df['Opp_kick_Punt_Rets']-df['Opp_kick_K/O_Rets']-offset,
+                marker={'color': 'Orange'},
+                hoverinfo='text',
+                text=['Team: {}<br>Missed Field Goal Returns Allowed: {}<br>Total Big Plays Plays Allowed: {}'
+                      .format(team, play, total) 
+                      for team, play, total in zip(df['Team'], df['Opp_kick_FGM_Ret'], df['Defensive_Cumsum'])],
+                showlegend=False
+            )
+        ]
+    )
+
+    fig.update_layout(
+        title='Big Play Analysis',
+        barmode='overlay',
+        xaxis=dict(
+            tickvals=tick_values,
+            ticktext=tick_labels,
+            showline=True,  # To show the y-axis line, which is the center line in this graph
+            linewidth=1,
+            linecolor='rgba(0, 0, 0, 0.5)',
+            zeroline=False,  # Hide the zero line
+        ),
+        yaxis=dict(
+            title='',  # No title for y-axis
+            showline=False,  # Show the y-axis line
+            showgrid=False,  # Turn off the grid lines
+            showticklabels=False
+        ),
+                # Set the plot's background color to white for the gap effect
+        plot_bgcolor='white',
+        annotations=[
+            # Offense Label
+            dict(
+                x=max_value / 2,  # Adjust this value as needed
+                y=1.1,  # Slightly above the top of the graph
+                xref='x',
+                yref='paper',
+                text='Offense (Big Plays Gained)',
+                showarrow=False,
+                font=dict(color='black', size=14),
+                align='center',
+            ),
+            # Defense Label
+            dict(
+                x=-max_value / 2,  # Adjust this value as needed
+                y=1.1,  # Slightly above the top of the graph
+                xref='x',
+                yref='paper',
+                text='Defense (Big Plays Allowed)',
+                showarrow=False,
+                font=dict(color='black', size=14),
+                align='center',
+            ),
+            ] + [
+                    # Annotations for team names or other static text
+            dict(
+                x=0,
+                y=idx,
+                text=team,
+                xref='x',
+                yref='y',
+                showarrow=False,
+                font=dict(color='black', size=12),
+                align='center',
+                xanchor='center',
+                yanchor='middle'
+            ) for idx, team in enumerate(df['Team'])
+        ]
+    )
+    return fig
 # Define the layout of the app
 app.layout = html.Div([
     dcc.Dropdown(
@@ -249,12 +577,41 @@ app.layout = html.Div([
     ),
     
     # Second chart (scatter or line chart)
-    dcc.Graph(id='first-down')
+    dcc.Graph(id='first-down'),
     
-    ])
-
-
+    dcc.Dropdown(
+        id='week-dropdown-agression',  # Unique ID for the second chart dropdown
+        options=[{'label': f'Week {i}', 'value': i} for i in range(1, 22)],  # Assuming 21 weeks
+        value=1  # Default value
+    ),
+    dcc.Graph(id='agression'),
+    
+    dcc.Dropdown(
+        id='week-dropdown-second-down',
+        options=[{'label': f'Week {i}', 'value': i} for i in range(1, 22)],  # Assuming 21 weeks
+        value=1  # Default value
+    ),
+    dcc.RadioItems(
+        id='heatmap-type',
+        options=[
+            {'label': 'Offense', 'value': 'offense'},
+            {'label': 'Defense', 'value': 'defense'}
+        ],
+        value='offense'  # Default value
+    ),
+    dcc.Graph(id='second-down'),
+    dcc.Graph(
+        id='big-play',
+        figure=create_big_play_analysis_graph()  # Call the function here
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+# In[ ]:
+
+
+
 
